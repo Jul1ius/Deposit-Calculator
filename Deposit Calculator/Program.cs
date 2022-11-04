277627e796427953076004d019f7c21a5e36df3a
nanji <nanji@DESKTOP-4A8MIDK>
2022-11-04 18:08:12 +0300

    Add project files.

---
+++ Deposit Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            double months = double.Parse(Console.ReadLine()); 
+            double percent = double.Parse(Console.ReadLine());
+
+            double tottal = deposit + months * ((deposit * percent) /12);
+
+            Console.WriteLine(tottal);
+        }
+    }
+}
